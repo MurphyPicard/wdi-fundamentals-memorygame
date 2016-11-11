ec388239cdea2d2eb12ba0aaf832f7856d2e8ad6
murphypicard <ayaghsizian@gmail.com>
2016-11-11 15:10:17 -0500

    created memory game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,8 @@
-console.log("JS file is connected to HTML! Woo!")
+window.onload = function(){
+console.log("JS file is connected to HTML! Woo!");
+};
+
+var cardOne = 'King';
+var cardTwo = 'King';
+var cardThree = 'Queen';
+var cardFour = 'Queen';
