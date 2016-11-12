a90c9ace8c9c9c479651806f5f53ba04c523723a
murphypicard <ayaghsizian@gmail.com>
2016-11-12 12:54:21 -0500

    alert

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,3 +6,6 @@ var cardOne = 'King';
 var cardTwo = 'King';
 var cardThree = 'Queen';
 var cardFour = 'Queen';
+
+if (cardTwo === cardFour){alert("You found a match!");}
+else if (cardTwo !== cardFour){alert("Sorry try again");}
